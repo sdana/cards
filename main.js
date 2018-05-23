917f5d2dfe00a14b9c9cdc22863e4c8fcc0f9d76
sdana <seth.dana@gmail.com>
2018-05-23 15:20:08 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,55 @@
+const cardDiv = document.querySelector(".card-div")
+const createButton = document.getElementById("create-button")
+const createBox = document.getElementById("create-box")
+let numberOfCards = cardDiv.childElementCount
+
+createButton.addEventListener("click", addCard)
+
+function addCard() {
+    let newCard = document.createElement("DIV")
+    newCard.setAttribute("class", "new-card")
+    let cardNumber = document.createAttribute("card-number")
+    newCard.setAttribute("card-number", numberOfCards)
+
+    let cardColorButton = document.createElement("BUTTON")
+    cardColorButton.innerHTML = "card color"
+    cardColorButton.addEventListener("click", changeCardColor)
+    cardDiv.appendChild(newCard).appendChild(cardColorButton)
+
+    let fontColorButton = document.createElement("BUTTON")
+    fontColorButton.innerHTML = "font color"
+    fontColorButton.addEventListener("click", changeFontColor)
+    cardDiv.appendChild(newCard).appendChild(fontColorButton)
+
+    let deleteButton = document.createElement("BUTTON")
+    deleteButton.innerHTML = "delete"
+    deleteButton.addEventListener("click", deleteCard)
+    cardDiv.appendChild(newCard).appendChild(deleteButton)
+    
+    let createBoxValue = document.createElement("P") 
+    createBoxValue.innerHTML = String(document.getElementById("create-box").value)
+    cardDiv.appendChild(newCard).appendChild(createBoxValue)
+    createBox.value = ""
+
+    console.log(numberOfCards)
+
+    return numberOfCards++
+}
+
+function changeCardColor(){
+    let colors = ["blue", "red", "green", "yellow"]
+    let cardNumber = parseInt(cardDiv.getAttribute("card-number"))
+    event.currentTarget.parentNode.style.backgroundColor = colors[parseInt(Math.floor(Math.random() * Math.floor(colors.length)))]
+    console.log(event.currentTarget.parentNode)
+}
+function changeFontColor() {
+    let colors = ["blue", "red", "green", "yellow", "black", "white"]
+    let cardNumber = parseInt(cardDiv.getAttribute("card-number"))
+    event.currentTarget.parentNode.style.color = colors[parseInt(Math.floor(Math.random() * Math.floor(colors.length)))]
+    console.log(event.currentTarget.parentNode)
+}
+function deleteCard(){
+    let whichCard = event.currentTarget.parentNode
+    console.log(whichCard)
+    whichCard.parentNode.removeChild(whichCard)
+}
