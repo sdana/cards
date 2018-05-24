526da36a39502ad2910f3a82f9a45e03b10ceee9
sdana <seth.dana@gmail.com>
2018-05-24 11:50:07 -0500

    added font and card color picker

--- main.js
+++ main.js
@@ -11,13 +11,16 @@ function addCard() {
     let cardNumber = document.createAttribute("card-number")
     newCard.setAttribute("card-number", numberOfCards)
 
-    let cardColorButton = document.createElement("BUTTON")
-    cardColorButton.innerHTML = "card color"
+    let cardColorButton = document.createElement("INPUT")
+    cardColorButton.setAttribute("type", "color")
+    cardColorButton.setAttribute("id", "colorPicker")
+    cardColorButton.setAttribute("value", "#1E90FF")
     cardColorButton.addEventListener("click", changeCardColor)
     cardDiv.appendChild(newCard).appendChild(cardColorButton)
 
-    let fontColorButton = document.createElement("BUTTON")
-    fontColorButton.innerHTML = "font color"
+    let fontColorButton = document.createElement("INPUT")
+    fontColorButton.setAttribute("type", "color")
+    fontColorButton.setAttribute("id", "fontColorPicker")
     fontColorButton.addEventListener("click", changeFontColor)
     cardDiv.appendChild(newCard).appendChild(fontColorButton)
 
@@ -36,28 +39,25 @@ function addCard() {
     return numberOfCards++
 }
 
-function changeCardColor(){
-    let colors = ["blue", "red", "green", "yellow"]
+const changeCardColor = () => {
     let cardNumber = parseInt(cardDiv.getAttribute("card-number"))
-    //choose a random color from colors array and assign to card
-    //event.currentTarget.parentNode.style.backgroundColor = colors[parseInt(Math.floor(Math.random() * Math.floor(colors.length)))]
-    
-    //This Code snipped generates a random HEX color value and assigns it to the card (I did not write this)
-    event.currentTarget.parentNode.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
+    let colorPicker = document.getElementById("colorPicker")
+    colorPicker.addEventListener("change", changeColor = () => {
+        event.currentTarget.parentNode.style.backgroundColor = colorPicker.value
+} )
+    event.currentTarget.parentNode.style.backgroundColor = colorPicker.value
     console.log(event.currentTarget.parentNode)
+
+
 }
-function changeFontColor() {
-    let colors = ["blue", "red", "green", "yellow", "black", "white"]
+const changeFontColor = () => {
     let cardNumber = parseInt(cardDiv.getAttribute("card-number"))
-    //event.currentTarget.parentNode.style.color = colors[parseInt(Math.floor(Math.random() * Math.floor(colors.length)))]
-    
-        //This Code snipped generates a random HEX color value and assigns it to the card (I did not write this)
-    event.currentTarget.parentNode.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16)
-    console.log(event.currentTarget.parentNode)
-    
-    console.log(event.currentTarget.parentNode)
+    let colorPicker = document.getElementById("fontColorPicker")
+    colorPicker.addEventListener("change", changeColor = () => {
+    event.currentTarget.parentNode.style.color = colorPicker.value
+    })
 }
-function deleteCard(){
+const deleteCard = () => {
     let whichCard = event.currentTarget.parentNode
     console.log(whichCard)
     whichCard.parentNode.removeChild(whichCard)
